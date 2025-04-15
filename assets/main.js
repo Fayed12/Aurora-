decc349fe7c2014a463d206c441b0b0d709ddc79
mohamed fayed <112355142+Fayed12@users.noreply.github.com>
2025-04-15 18:04:24 +0200

    Add files via upload

---
+++ assets/main.js
@@ -0,0 +1,4 @@
+const bars = document.getElementById("bar").addEventListener("click" , function(){
+    document.getElementById("nav-bar").style.display="block";
+    document.getElementById("nav-bar").style.position="absolute";
+})
