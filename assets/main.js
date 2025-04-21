b70a6a1a9e97d77fc275d6a7950c0f823b3ce128
mohamed fayed <112355142+Fayed12@users.noreply.github.com>
2025-04-22 00:44:23 +0200

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,20 @@
 const bars = document.getElementById("bar").addEventListener("click" , function(){
     document.getElementById("nav-bar").classList.toggle("show");
-    document.getElementById("nav-bar").style.position="absolute";
+    // document.getElementById("nav-bar").style.position="absolute";
 });
-
 let scrolling = document.getElementById("links-bar");
 scrolling.addEventListener("mouseover" , function(){
     scrolling.style.overflowY="auto";
 })
-scrolling.addEventListener("mouseleave" , function(){
+scrolling.addEventListener("mouseout" , function(){
     scrolling.style.overflowY="hidden";
 })
 
+// table scroll bar
+let tablescroll = document.getElementById("table-scroller");
+tablescroll.addEventListener("mouseover" , function(){
+    tablescroll.style.overflowX="auto";
+})
+tablescroll.addEventListener("mouseout" , function(){
+    tablescroll.style.overflowX="hidden";
+})
