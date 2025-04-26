1e7d3d5821387448aedf80af2a424e94752d6524
mohamed fayed <112355142+Fayed12@users.noreply.github.com>
2025-04-26 15:47:10 +0300

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,5 @@
 const bars = document.getElementById("bar").addEventListener("click" , function(){
     document.getElementById("nav-bar").classList.toggle("show");
-    // document.getElementById("nav-bar").style.position="absolute";
 });
 let scrolling = document.getElementById("links-bar");
 scrolling.addEventListener("mouseover" , function(){
