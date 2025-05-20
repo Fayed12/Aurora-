e1d2e88438d978855a4bc39d66abaee5e30dec02
mohamed fayed <112355142+Fayed12@users.noreply.github.com>
2025-05-20 18:50:18 +0300

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,7 @@
-const bars = document.getElementById("bar").addEventListener("click" , function(){
+// functionalty of main side bar
+const bars = document.getElementById("bar").addEventListener("click", function () {
     document.getElementById("nav-bar").classList.toggle("show");
+    document.getElementById("Overlay").classList.toggle("overlay");
 });
 let scrolling = document.getElementById("links-bar");
 scrolling.addEventListener("mouseover" , function(){
@@ -9,7 +11,24 @@ scrolling.addEventListener("mouseout" , function(){
     scrolling.style.overflowY="hidden";
 })
 
-// table scroll bar
+// ===============================================================================================================
+
+// Product roadmap visible and hidden
+let tableHeader = document.querySelectorAll(".arrowUp");
+let auroraDetails = document.querySelectorAll(".auroraDetails");
+let icon = document.querySelectorAll(".icon");
+
+tableHeader.forEach((header, index) => {
+    header.addEventListener("click", () => {
+        auroraDetails[index].classList.toggle("hiddentable");
+        auroraDetails[index].classList.toggle("aurora-details");
+        icon[index].classList.toggle("rotate");
+    });
+});
+
+// ===============================================================================================================
+
+//functionalty of  table scroll bar
 let tablescroll = document.getElementById("table-scroller");
 tablescroll.addEventListener("mouseover" , function(){
     tablescroll.style.overflowX="auto";
